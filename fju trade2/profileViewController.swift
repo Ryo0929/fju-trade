286dbe315e13bca70851d20fbb8a0b242d1e2341
PC_18 <pc_18@155-195.im.fju.edu.tw>
2018-01-06 17:08:32 +0800

    profile

---
+++ fju trade2/profileViewController.swift
@@ -0,0 +1,48 @@
+//
+//  profileViewController.swift
+//  fju trade2
+//
+//  Created by PC_18 on 2018/1/6.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+import FirebaseAuth
+class profileViewController: UIViewController {
+    let currentuserid=Auth.auth().currentUser?.uid
+    @IBOutlet weak var id: UILabel!
+    @IBOutlet weak var phone: UILabel!
+    @IBOutlet weak var line: UILabel!
+    @IBOutlet weak var fb: UILabel!
+    override func viewDidLoad() {
+        let tryDatabase = Database.database().reference()
+        tryDatabase.child("account").child(currentuserid!).observeSingleEvent(of: .value){ (snapshot) in
+         let value = snapshot.value as? NSDictionary
+            self.id.text=value?["name"] as? String ?? ""
+            self.phone.text=value?["phone"] as? String ?? ""
+            self.line.text=value?["line"] as? String ?? ""
+            self.fb.text=value?["name"] as? String ?? ""
+        }
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
